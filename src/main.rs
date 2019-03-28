9b41fd830b805e70758e834e4a91b60cb26d8457
Paul Naranja <pnaranja@chegg.com>
2019-03-27 23:23:52 -0700

    Add rodio crate to play music

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::fs::File;
+use std::io::BufReader;
 use std::thread;
 use std::time;
 
+use rodio::Source;
+
 fn play_music() {
-    std::iter::repeat(0).for_each(|_| {
-        println!("Playing music!");
-        thread::sleep(time::Duration::from_millis(500));
-    });
+    let file_loc = "sounds/Thunder_HeavyRain_Wind.mp3";
+    let device = rodio::default_output_device().unwrap();
+
+    let file = File::open(file_loc).unwrap();
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+    rodio::play_raw(&device, source.convert_samples());
+
 }
 
 fn main() {
