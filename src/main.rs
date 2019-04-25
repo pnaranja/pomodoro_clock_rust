a55e4b903cf99783d49aff5ca25c931baf23d500
Paul Naranja <pnaranja@chegg.com>
2019-04-24 23:51:25 -0700

    Allow user to enter time to play in seconds and mp3 file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::env::args;
 use std::fs::File;
 use std::io::BufReader;
+use std::process::exit;
+use std::str::FromStr;
 use std::thread;
 use std::time;
 
 use rodio::Source;
 
-fn play_music() {
-    let file_loc = "sounds/Thunder_HeavyRain_Wind.mp3";
+fn handle_args(args: &Vec<String>) {
+    if args.len() == 1 {
+        println!("USAGE: pomodoro_clock_rust <length_in_secs> <mp3>");
+        exit(1);
+    }
+}
+
+fn play_music(file_loc: String) {
     let device = rodio::default_output_device().unwrap();
 
     let file = File::open(file_loc).unwrap();
@@ -16,7 +25,13 @@ fn play_music() {
 }
 
 fn main() {
-    thread::spawn(play_music);
-    thread::sleep(time::Duration::from_secs(5));
+    let args: Vec<String> = args().collect();
+    handle_args(&args);
+
+    let length_in_secs = u64::from_str(&args[1]).unwrap();
+    let mp3_loc: String = args.get(2).unwrap().to_owned();
+
+    thread::spawn(|| play_music(mp3_loc));
+    thread::sleep(time::Duration::from_secs(length_in_secs));
     println!("Playing ALARM!!!!")
 }
